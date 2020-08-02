4510d932ad74ca9fd93454f87862e1bd4a4a51a9
Rayhaaan Sahib <rayhaanus@gmail.com>
2020-08-02 13:10:00 +1200

    Add project files.

---
+++ FormMain.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Rename_Image_Files
+{
+    partial class FormRename
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.BtnSelect = new System.Windows.Forms.Button();
+            this.LblPath = new System.Windows.Forms.Label();
+            this.BtnRename = new System.Windows.Forms.Button();
+            this.gbDetails = new System.Windows.Forms.GroupBox();
+            this.LVFilenames = new System.Windows.Forms.ListView();
+            this.CLBFileType = new System.Windows.Forms.CheckedListBox();
+            this.gbDetails.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // BtnSelect
+            // 
+            this.BtnSelect.Location = new System.Drawing.Point(21, 12);
+            this.BtnSelect.Name = "BtnSelect";
+            this.BtnSelect.Size = new System.Drawing.Size(75, 23);
+            this.BtnSelect.TabIndex = 0;
+            this.BtnSelect.Text = "Select &Folder";
+            this.BtnSelect.UseVisualStyleBackColor = true;
+            this.BtnSelect.Click += new System.EventHandler(this.BtnSelect_Click);
+            // 
+            // LblPath
+            // 
+            this.LblPath.AutoSize = true;
+            this.LblPath.Location = new System.Drawing.Point(6, 27);
+            this.LblPath.Name = "LblPath";
+            this.LblPath.Size = new System.Drawing.Size(42, 13);
+            this.LblPath.TabIndex = 1;
+            this.LblPath.Text = "Folder: ";
+            // 
+            // BtnRename
+            // 
+            this.BtnRename.Location = new System.Drawing.Point(163, 12);
+            this.BtnRename.Name = "BtnRename";
+            this.BtnRename.Size = new System.Drawing.Size(75, 23);
+            this.BtnRename.TabIndex = 2;
+            this.BtnRename.Text = "&Rename";
+            this.BtnRename.UseVisualStyleBackColor = true;
+            this.BtnRename.Click += new System.EventHandler(this.BtnRename_Click);
+            // 
+            // gbDetails
+            // 
+            this.gbDetails.AutoSize = true;
+            this.gbDetails.Controls.Add(this.LVFilenames);
+            this.gbDetails.Controls.Add(this.LblPath);
+            this.gbDetails.Location = new System.Drawing.Point(12, 51);
+            this.gbDetails.Name = "gbDetails";
+            this.gbDetails.Size = new System.Drawing.Size(237, 295);
+            this.gbDetails.TabIndex = 4;
+            this.gbDetails.TabStop = false;
+            this.gbDetails.Text = "Details";
+            // 
+            // LVFilenames
+            // 
+            this.LVFilenames.Alignment = System.Windows.Forms.ListViewAlignment.Default;
+            this.LVFilenames.AllowColumnReorder = true;
+            this.LVFilenames.HideSelection = false;
+            this.LVFilenames.Location = new System.Drawing.Point(9, 44);
+            this.LVFilenames.Name = "LVFilenames";
+            this.LVFilenames.Size = new System.Drawing.Size(217, 232);
+            this.LVFilenames.TabIndex = 2;
+            this.LVFilenames.UseCompatibleStateImageBehavior = false;
+            this.LVFilenames.View = System.Windows.Forms.View.List;
+            // 
+            // CLBFileType
+            // 
+            this.CLBFileType.FormattingEnabled = true;
+            this.CLBFileType.Location = new System.Drawing.Point(269, 51);
+            this.CLBFileType.Name = "CLBFileType";
+            this.CLBFileType.Size = new System.Drawing.Size(120, 94);
+            this.CLBFileType.TabIndex = 5;
+            // 
+            // FormRename
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 348);
+            this.Controls.Add(this.CLBFileType);
+            this.Controls.Add(this.gbDetails);
+            this.Controls.Add(this.BtnRename);
+            this.Controls.Add(this.BtnSelect);
+            this.Name = "FormRename";
+            this.Text = "Rename Image Files";
+            this.gbDetails.ResumeLayout(false);
+            this.gbDetails.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button BtnSelect;
+        private System.Windows.Forms.Label LblPath;
+        private System.Windows.Forms.Button BtnRename;
+        private System.Windows.Forms.GroupBox gbDetails;
+        private System.Windows.Forms.ListView LVFilenames;
+        private System.Windows.Forms.CheckedListBox CLBFileType;
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Windows.Forms;
+using System.Windows.Media.Imaging;
+
+namespace Rename_Image_Files
+{
+    public partial class FormRename : Form
+    {
+        ArrayList fileList = new ArrayList();
+        string[] fileType = new string[] { "JPG", "PNG", "MOV", "3GP","MP4" };
+        string directory;
+        int numFiles;
+        string logFile = "C:\\Test\\" + DateTime.Now.ToString("yyyMMdd") + ".log";
+
+
+
+        public enum xFileType
+        {
+            JPG, PNG, MOV
+        };
+
+        public FormRename()
+        {
+            InitializeComponent();
+            FileTypeToCheckBox();
+        }
+
+
+        public void FileTypeToCheckBox()
+        {
+            try
+            {
+                foreach (string value in fileType)
+                {
+                    CLBFileType.Items.Add(value, true);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "ERROR");
+                UpdateLog(e.Message);
+            }
+        }
+
+        private void BtnSelect_Click(object sender, EventArgs e)
+        {
+            directory = string.Empty;
+            LVFilenames.Clear();
+            Console.Clear();
+
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                directory = Path.GetDirectoryName(dialog.FileName);
+                LblPath.Text = "Folder: " + directory;
+                UpdateLog(LblPath.Text.ToUpper());
+                LoadFilestoArray(directory);
+            }
+        }
+
+        private void LoadFilestoArray(string directory)
+        {
+            fileList.Clear();
+
+            if (!string.IsNullOrEmpty(directory))
+            {
+                UpdateLog("LOADING FILES TO LIST");
+
+                foreach (int checkedIndex in CLBFileType.CheckedIndices)
+                {
+                    string ext = "*." + CLBFileType.Items[checkedIndex].ToString();
+                    //Console.WriteLine(checkedIndex + "\t" + CLBFileType.Items[checkedIndex].ToString());
+
+                    fileList.AddRange(System.IO.Directory.GetFiles(directory, ext));
+                }
+                numFiles = fileList.Count;
+
+                UpdateListView();
+            }
+        }
+
+
+        private void UpdateListView()
+        {
+            if (numFiles != 0)
+            {
+                int count = 0;
+
+                UpdateLog("UPLOADING FOLDER LIST");
+
+                while (count < numFiles)
+                {
+                    string name = Path.GetFileName(fileList[count].ToString());
+                    LVFilenames.Items.Add(name);
+                    UpdateLog(name);
+                    count += 1;
+                }
+            }
+        }
+
+        private void UpdateFileName()
+        {
+            int count = 0;
+            DateTime dtCreated = DateTime.MinValue;
+            int updated = 0;
+            string strNewFileName;
+
+            string logText = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss") + "\t" + LblPath.Text.ToUpper();
+
+            UpdateLog(logText);
+            UpdateLog("RENAMING FILES");
+
+            while (count < numFiles)
+            {
+
+                string strCurFile = fileList[count].ToString();
+                string ext = Path.GetExtension(strCurFile);
+
+                if (strCurFile == "C:\\test\\20200111_123542.JPG")
+                {
+                    MessageBox.Show(strCurFile);
+                }
+
+                FileStream fsCurFile = new FileStream(strCurFile, FileMode.Open);
+
+                if (IsImageFile(ext))
+                {
+                    BitmapSource bmsource = BitmapFrame.Create(fsCurFile);
+                    BitmapMetadata filemeta = (BitmapMetadata)bmsource.Metadata;
+
+                    if (!string.IsNullOrEmpty(filemeta.DateTaken))
+                    {
+                        dtCreated = DateTime.Parse(filemeta.DateTaken);
+                    }
+                }
+                else
+                {
+                    FileInfo x = new FileInfo(strCurFile);
+                    dtCreated = x.LastWriteTime;
+                }
+
+                //if (!string.IsNullOrEmpty(dtCreated.ToString()))
+                if (dtCreated != DateTime.MinValue)
+                {
+                    strNewFileName = directory + "\\" + dtCreated.ToString("yyyyMMdd_HHmmss") + ext;
+
+
+                    //IF THE FILE HAS A NEW NAME
+                    if (!String.Equals(strCurFile, strNewFileName, StringComparison.OrdinalIgnoreCase))
+                    {
+                        bool fileExists = File.Exists(strNewFileName);
+
+                        //IF NEW FILE NAME DOESNT EXIST, RENAME CURRENT FILE
+                        if (!fileExists)
+                        {
+                            {
+                                try
+                                {
+                                    fsCurFile.Close(); //Close or else File.Move will not work
+                                    File.Move(strCurFile, strNewFileName);
+
+                                    logText = Path.GetFileName(strCurFile) + "\t" + ">>" + "\t" + Path.GetFileName(strNewFileName);
+                                    UpdateLog(logText);
+                                }
+                                catch (Exception e)
+                                {
+                                    logText = Path.GetFileName(strCurFile) + "\t" + "File not moved. Error: " + e.Message;
+                                    UpdateLog(logText.ToUpper());
+                                }
+                            }
+                        }
+
+                        //NEW FILE EXISTS
+                        else
+                        {
+                            try
+                            {
+                                string fileNameOnly = Path.GetFileNameWithoutExtension(strNewFileName);
+                                int fileNumber = 0;
+
+                                do
+                                {
+                                    fileNumber += 1;
+                                    strNewFileName = directory + fileNameOnly + "(" + fileNumber + ")" + ext;
+                                } while (File.Exists(strNewFileName));
+
+                                fsCurFile.Close();//Close or else File.Move will not work
+                                                  //File.Create(strNewFileName);
+                                File.Move(strCurFile, strNewFileName);
+
+                                logText = Path.GetFileName(strCurFile) + "\t" + ">>" + "\t" + Path.GetFileName(strNewFileName);
+                                UpdateLog(logText.ToUpper());
+                            }
+                            catch (Exception e)
+                            {
+                                logText = Path.GetFileName(strCurFile) + "\t" + e.Message;
+                                UpdateLog(logText);
+                            }
+                        }
+                        updated += 1;
+                    }
+                }
+                count += 1;
+                fsCurFile.Close();
+            }
+
+            string msg = "FILES UPDATED:" + "\t" + updated.ToString();
+            MessageBox.Show(msg, "Update Stats");
+            UpdateLog(msg);
+        }
+
+        private void BtnRename_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(directory))
+            {
+                MessageBox.Show("Select a folder", "Error");
+            }
+            else
+            {
+                fileList.Clear();
+                LVFilenames.Clear();
+
+                LoadFilestoArray(directory);
+                UpdateFileName();
+            }
+        }
+
+        /// <summary>
+        /// Check if file is an Image
+        /// </summary>
+        /// <param name="fileExtension"> Filename extension</param>
+        private bool IsImageFile(string fileExtension)
+        {
+            //foreach (var fileType in Enum.GetValues(typeof(FileType)))
+            foreach (string value in fileType)
+            {
+                if (fileExtension.ToUpper() == ".JPG" && value == "JPG")
+                {
+                    return true;
+                }
+                else if (fileExtension.ToUpper() == ".PNG" && value == "PNG")
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void UpdateLog(string message)
+        {
+            string log = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss") + "\t" + message;
+            File.AppendAllText(logFile, log + Environment.NewLine);
+            Console.WriteLine(log);
+        }
+    }
+}
